d128a543a7ae00ab268aee5361d6b3345da4ceb1
Avital <avitalg91@gmail.com>
2019-04-13 01:04:23 +0300

    add 404

---
+++ src/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="err-404">
+    404 page
+  </div>
+)
+
+export default Error;
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Home.js';
 import About from './About.js';
 import Room from './Room.js';
+import Error from './404.js';
 
 const Main = () => (
   <main>
@@ -10,6 +11,8 @@ const Main = () => (
       <Route exact path='/' component={Home}/>
       <Route exact path='/about' component={About}/>      
       <Route exact path='/room/:number' component={Room}/>
+      <Route exact path='/404' component={Error}/>
+      <Redirect to='/404'/>
     </Switch>
   </main>
 )
